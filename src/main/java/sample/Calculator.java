127db78b6c1c92c7108adce34862beadf38c97f9
Anthony Wingo <anthony@Anthonys-MacBook-Pro.local>
2018-01-24 17:35:32 -0600

    Begin Project

---
+++ src/main/java/sample/Calculator.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to the calculator");
+
+    }
+}
+
---
+++ src/main/java/sample/miles_calculations.java
@@ -0,0 +1,37 @@
+package sample;
+import java.util.Scanner;
+
+
+
+public class miles_calculations {
+
+    public static void main(String[] args){
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello and welcome to the miles calculations");
+        boolean mainLoop = true;
+        int choice;
+        while(true){
+            System.out.println("Estimated travel times \n");
+            System.out.println("1.) Travel by walking \n");
+            System.out.println("2.) Travel by car \n");
+            System.out.println("4.) Exit");
+            System.out.println("Enter you Desired method of travel");
+
+            choice = input.nextInt();
+
+
+            switch (choice){
+                case 1:
+
+
+
+            }
+        }
+
+
+}
+
+
+}
